9ebd3a2b4dc2d793668aab9636d27a648e409baf
DarkCompet <darkcompet@gmail.com>
2022-02-23 15:19:55 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Photon for Core
+
+This provides Photon streaming for Core.
+
+- Features
+
+	```bash
+	- Streaming.
+	```
+
+- Dependencies
+
+	```bash
+	# Tool.Compet.Log
+	https://github.com/darkcompet/csharp-unity-log
+	
+	# Tool.Compet.Core
+	https://github.com/darkcompet/csharp-core
+	```
+
+
+## How to install
+
+- Just include it into project by add as git-submodule.
+	
+	```bash
+	# Tool.Compet.Photon
+	git submodule add https://github.com/darkcompet/csharp-core-photon.git
+	```
---
+++ src/DkPhotonMessageType.cs
@@ -0,0 +1,21 @@
+namespace Tool.Compet.Photon {
+	/// Declare type for message in communication between server and client.
+	/// Values in range [0 -> 255]
+	public class DkPhotonMessageType {
+		/// Indicates the message targets to call a service methods
+		/// which be declared inside service interface (shared between server and client).
+		/// Both server and client must implement service methods for communication.
+		///
+		/// In general, we use this type when we want server side implements some logic for us
+		/// like `Join to a room, Leave from a room,...`.
+		public const byte SERVICE = 1;
+
+		/// This is like as `SERVICE` type, but server side does not know/implement
+		/// service methods which be declared at client side.
+		/// Only client needs to implement service methods which be annotated with `DkPhotonRPC`.
+		///
+		/// It is convenience when we want to communicate between clients only, and don't need
+		/// server side implementation.
+		public const byte RPC = 2;
+	}
+}
