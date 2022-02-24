8c6eebcc9557cf12637fbc703ef5743a3523b513
DarkCompet <darkcompet@gmail.com>
2022-02-24 15:27:28 +0700

    Add client target for RPC method

---
+++ src/DkPhotonClientTarget.cs
@@ -0,0 +1,9 @@
+namespace Tool.Compet.Photon {
+	public enum DkPhotonClientTarget {
+		/// Send to all players (include me).
+		ALL,
+
+		/// Only send to other players (exclude me).
+		OTHERS,
+	}
+}
--- src/DkPhotonMessageType.cs
+++ src/DkPhotonMessageType.cs
@@ -1,14 +1,14 @@
 namespace Tool.Compet.Photon {
 	/// Declare type for message in communication between server and client.
 	/// Values in range [0 -> 255]
-	public class DkPhotonMessageType {
+	public enum DkPhotonMessageType {
 		/// Indicates the message targets to call a service methods
 		/// which be declared inside service interface (shared between server and client).
 		/// Both server and client must implement service methods for communication.
 		///
 		/// In general, we use this type when we want server side implements some logic for us
 		/// like `Join to a room, Leave from a room,...`.
-		public const byte SERVICE = 1;
+		SERVICE,
 
 		/// This is like as `SERVICE` type, but server side does not know/implement
 		/// service methods which be declared at client side.
@@ -16,6 +16,6 @@ namespace Tool.Compet.Photon {
 		///
 		/// It is convenience when we want to communicate between clients only, and don't need
 		/// server side implementation.
-		public const byte RPC = 2;
+		RPC,
 	}
 }
